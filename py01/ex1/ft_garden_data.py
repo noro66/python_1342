dd14d205ec08307a514499abd67d8f51a4d607b6
Nouaamane  Ait-sfia <nait-sfi@e2r2p15.1337.ma>
2025-12-25 17:07:37 +0100

    add: first ex in python02

---
+++ py01/ex1/ft_garden_data.py
@@ -0,0 +1,19 @@
+class Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+
+
+rose_obj = Plant("Rose", 25, 30)
+sunflower_obj = Plant("Sunflower", 80, 45)
+cactus_obj = Plant("Cactus", 15, 120)
+
+
+def print_plant_info(plant: Plant) -> None:
+    print(f"{plant.name.capitalize()}: {plant.height}cm, {plant.age} days old")
+
+
+print("=== Garden Plant Registry ===")
+for obj in (rose_obj, sunflower_obj, cactus_obj):
+    print_plant_info(obj)

538d707357f6ec7b99a75c207a68713a0e87198c
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-01-12 13:43:52 +0100

    add: ex3 on python 04

---
+++ py04/ex3/ft_vault_security.py
@@ -0,0 +1,26 @@
+def vault_security():
+    print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
+    try:
+        print("Initiating secure vault access...")
+        with open("classified_data.txt", 'r') as classified_data:
+            print("Vault connection established with failsafe protocols")
+            print()
+            print("SECURE EXTRACTION:")
+            content = classified_data.read()
+            print(content)
+        with open("security_protocols.txt", 'w') as security_protocols:
+            security_protocols.write(
+                "[CLASSIFIED] New security protocols archived"
+                )
+            print()
+            print("SECURE PRESERVATION:")
+            print("[CLASSIFIED] New security protocols archived")
+            print("Vault automatically sealed upon completion")
+            print()
+        print("All vault operations completed with maximum security.")
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+
+if __name__ == "__main__":
+    vault_security()

68db1142f93d424e5fd224e1b7339670a9332203
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-01-20 11:28:12 +0100

    add: a semulation of a hash table

---
+++ fcc/Build_a_Hash_Table/hash_table.py
@@ -0,0 +1,29 @@
+class HashTable:
+    def __init__(self) -> None:
+        self.collection = {}
+
+    def hash(self, string: str):
+        res = []
+        for s in string:
+            res.append(ord(s))
+        return sum(res)
+
+    def add(self, key, value):
+        hashed_key = self.hash(key)
+        if hashed_key not in self.collection:
+            self.collection[hashed_key] = {key: value}
+        else:
+            self.collection[hashed_key][key] = value
+
+    def remove(self, key):
+        hashed_key = self.hash(key)
+        if hashed_key in self.collection:
+            if key in self.collection[hashed_key]:
+                del self.collection[hashed_key][key]
+
+    def lookup(self, key):
+        hashed_key = self.hash(key)
+        if hashed_key in self.collection:
+            if key in self.collection[hashed_key]:
+                return self.collection[hashed_key][key]
+        return None

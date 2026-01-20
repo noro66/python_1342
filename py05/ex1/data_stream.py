191c1af9e365ad0b9d85b1f6ba58e06990e4162c
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-01-20 15:53:36 +0100

    add: transation stream processing buys and sells

--- py05/ex1/data_stream.py
+++ py05/ex1/data_stream.py
@@ -1,3 +1,4 @@
+import sys
 from abc import ABC, abstractmethod
 from typing import List, Any, Optional, Dict, Union
 
@@ -43,7 +44,7 @@ class SensorStream(DataStream):
         try:
             self.avg_temp = total_temp / temp_val_count
         except ZeroDivisionError as e:
-            print("ERROR: ", e)
+            print("ERROR: ", e, file=sys.stderr)
             return ""
 
         self.stats["reading_process"] = r_process
@@ -59,5 +60,41 @@ class SensorStream(DataStream):
                     dictionary
                     for dictionary in data_batch
                     if criteria in dictionary
+                    ]
+        return data_batch
+
+
+class TransactionStream(DataStream):
+    def __init__(self, stream_id: str) -> None:
+        super().__init__(stream_id)
+        self.stats["buy"] = 0
+        self.stats["sell"] = 0
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        try:
+            t_ops = net_flow = 0
+            for dictionary in data_batch:
+                t_ops += 1
+                type_value = dictionary.get("type", "")
+                amount_value = dictionary.get("amount", 0)
+                if type_value == "buy":
+                    net_flow += amount_value
+                    self.stats["buy"] += amount_value
+                elif type_value == "sell":
+                    net_flow -= amount_value
+                    self.stats["sell"] += amount_value
+            return (
+                f"Transaction analysis: {t_ops}, net flow: {net_flow} units"
+                )
+        except Exception as e:
+            print("ERROR: ", e, file=sys.stderr)
+            return ""
+
+    def filter_data(self, data_batch: List[Any], criteria: str
+                    | None = None) -> List[Any]:
+        if criteria:
+            return [dictionary
+                    for dictionary in data_batch
+                    if dictionary.get("type", "") == criteria
                     ]
         return data_batch

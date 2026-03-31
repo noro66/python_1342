b20746e434dd3186790b7a159c8b227c19602ab4
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-03-31 13:17:10 +0100

    add: doubly linked list

---
+++ data_structures/doubly_linked_list.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self, value) -> None:
+        node = Node(value)
+        self.head = node
+        self.tail = node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            new_node = self.head
+            new_node = self.tail
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+
+
+my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
+
+
+print('Head:', my_doubly_linked_list.head.value)
+print('Tail:', my_doubly_linked_list.tail.value)
+print('Length:', my_doubly_linked_list.length, '\n')
+
+print('Doubly Linked List:')
+my_doubly_linked_list.print_list()

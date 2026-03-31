ee35feba38f4799714f53585d1a0f9bb41b677ed
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-03-31 13:37:05 +0100

    add: pop from a doubly linked list

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -28,15 +28,36 @@ class DoublyLinkedList:
             new_node.prev = self.tail
             self.tail = new_node
         self.length += 1
+        return True
+
+    def pop(self):
+        if not self.head:
+            return None
+        last_node = None
+        if self.length == 1:
+            last_node = self.head
+            last_node.prev = None
+            last_node.next = None
+            self.head = None
+            self.tail = None
+        else:
+            last_node = self.tail
+            self.tail = self.tail.prev
+            self.tail.next = None
+            last_node.prev = None
+        self.length -= 1
+        return last_node
 
 
 my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
 
 
-print('Head:', my_doubly_linked_list.head.value)
-print('Tail:', my_doubly_linked_list.tail.value)
-print('Length:', my_doubly_linked_list.length, '\n')
+# (2) Items - Returns 2 Node
+print(my_doubly_linked_list.pop().value)
+# (1) Item -  Returns 1 Node
+print(my_doubly_linked_list.pop().value)
+# (0) Items - Returns None
+print(my_doubly_linked_list.pop())
 
-print('Doubly Linked List:')
-my_doubly_linked_list.print_list()
+

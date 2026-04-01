97ed78d357da93d48c463db8a05f21eaf7a04d05
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-04-01 11:51:54 +0100

    add: enqueue the  queue

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -3,10 +3,21 @@ class Node:
         self.value = value
         self.next = None
 
-        
+
 class Queue:
     def __init__(self, value) -> None:
         new_node = Node(value)
         self.first = new_node
         self.last = new_node
         self.length = 1
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length <= 0:
+            self.first = new_node
+            self.last = new_node
+            return True
+        self.last.next = new_node
+        self.last = new_node
+        self.length += 1
+        return True

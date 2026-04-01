5b390a7de648747545f96f3ee420628f5d383394
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-04-01 11:10:08 +0100

    add: push to the stack

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -23,8 +23,23 @@ class Stack:
         self.height += 1
         return True
 
+    def pop(self, value):
+        if not self.top:
+            return none
+        tmp = self.top
+        self.top = tmp.next
+        self.height -= 1
 
 my_stack = Stack(4)
+my_stack.push(3)
+my_stack.push(2)
+my_stack.push(1)
 
-print('Top:', my_stack.top.value)
-print('Height:', my_stack.height)
+print('Stack before pop():')
+my_stack.print_stack()
+
+print('\nPopped node:')
+print(my_stack.pop().value)
+
+print('\nStack after pop():')
+my_stack.print_stack()

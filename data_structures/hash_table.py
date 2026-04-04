5585c5a257b3ff9c140932d329f12f5f7230e36e
Nouaamane  Ait-sfia <norou6@gmail.com>
2026-04-04 09:26:12 +0100

    add: hash table, get keys

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -26,12 +26,10 @@ class HashTable:
                     return self.data_map[index][i][1]
         return None
 
-
-my_hash_table = HashTable()
-
-my_hash_table.set_item('bolts', 1400)
-my_hash_table.set_item('washers', 50)
-
-print('Bolts:', my_hash_table.get_item('bolts'))
-print('Washers:', my_hash_table.get_item('washers'))
-print('Lumber:', my_hash_table.get_item('lumber'))
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
